dd7726394f0afba5324b70aefd2a38dc96faafbf
nerjan <jan.belkner@gmail.com>
2020-09-28 00:48:05 +0200

    all the foods

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,11 +4,59 @@ import Pacman from '../Pacman';
 import Ghost from '../Ghost';
 import Food from '../Food'
 class Board extends Component {
+
+
+    constructor(props){
+        super(props);
+
+        this.pacmanRef = React.createRef();
+
+        this.foods = [];
+        this.amountOfFood = (
+            (window.innerWidth - this.props.foodSize) * (window.innerHeight - this.props.topScoreBoardHeight)/ (this.props.foodSize * this.props.foodSize) - 1
+        );
+        console.log(this.amountOfFood)
+        for (let i=0; i < this.amountOfFood; i++) {
+            this['food' + i] = React.createRef();
+        }
+    }
+
+
+    lookForEat = () => {
+
+    }
+
+    
     render() {
+        const {foodSize, border, topScoreBoardHeight} = this.props
+        let foods = [];
+        let currentTop = 0;
+        let currentLeft = 1 * foodSize;
+        for (let i = 0; i < this.amountOfFood; i++) {
+            if (currentLeft + foodSize >= window.innerWidth - border) {
+                currentTop += foodSize;
+                currentLeft = 0;
+            }
+
+            if (currentTop + foodSize >= window.innerHeight - border - topScoreBoardHeight) {
+                break;
+            }
+
+            const position = {left: currentLeft, top: currentTop};
+            currentLeft += foodSize;
+            foods.push(
+                <Food 
+                key={`food-elem=${i}`} 
+                position={position} 
+                ref={this['food' + i]}
+                />)
+        }
+
+
         return (
         <div className="board">
-            <Food position={{top:100, left:100}}/>
-            <Pacman/>
+            {foods}
+            <Pacman ref={this.pacmanRef}/>
             <Ghost  color="red"/>
             <Ghost  color="yellow"/>
             <Ghost  color="blue"/>
@@ -17,6 +65,12 @@ class Board extends Component {
         </div>
         )
     }
+}
+
+Board.defaultProps = {
+    foodSize: 50,
+    border: 10 *2,
+    topScoreBoardHeight: 50
 }
 
 export default Board;

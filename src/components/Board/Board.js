d40fd28571ea62edc9414db4077e895572bd0b5d
nerjan <jan.belkner@gmail.com>
2020-09-27 14:48:49 +0200

    pacman the beginning

---
+++ src/components/Board/Board.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './style.css';
+
+
+class Board extends Component {
+    render() {
+        return (
+        <div className="board">
+            <div>Hello</div>
+            /* <Pacman /> */
+        </div>
+        )
+    }
+}
+
+export default Board;

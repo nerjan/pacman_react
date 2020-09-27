4c9ef0c68088ecb80195a0cbbeb76ae4bfa4ecc6
nerjan <jan.belkner@gmail.com>
2020-09-27 20:07:20 +0200

    pacman movement

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -6,7 +6,7 @@ class Pacman extends Component {
     state = {
         direction: 'right',
         position : {
-            top: 60,
+            top: 0,
             left: 0,
 
         }
@@ -23,7 +23,53 @@ class Pacman extends Component {
 
     handleKeyDown  = (event) => {
         console.log(event.keyCode, event.key);
+        const currentTop = this.state.position.top;
+        const currentLeft = this.state.position.left;
+        const {step} = this.props;
+        console.log(this.state.direction);
 
+        // 39 ArrowRight
+        // 40 ArrowDown
+        // 37 ArrowLeft
+        // 38 ArrowUp
+        if (event.key === 'ArrowUp') {
+            this.setState({
+                position:{
+                    top: currentTop - step,
+                    left: currentLeft
+                },
+                direction: 'up'
+
+
+            });
+        }else if (event.key === 'ArrowRight'){
+            this.setState({
+                direction: 'right',
+                position:{
+                    top: currentTop,
+                    left: currentLeft + step
+                }
+
+            });
+        }else if (event.key === 'ArrowDown'){
+            this.setState({
+                direction: 'down',
+                position:{
+                    top: currentTop + step,
+                    left: currentLeft
+                }
+
+            });
+        }else if (event.key === 'ArrowLeft'){
+            this.setState({
+                direction: 'left',
+                position:{
+                    top: currentTop,
+                    left: currentLeft - step
+                }
+
+            });
+        }
     }
     render() {
         const {direction, position } = this.state;
